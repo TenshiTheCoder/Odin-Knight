4b0f7d8dfb3ea5fdb6f865c60c4d26082019734f
TenshiTheCoder <tenshibusinessbrand@gmail.com>
2026-09-25 04:22:22 -0400

    Implement BFS pathfinding

--- knight.js
+++ knight.js
@@ -1,6 +1,55 @@
 const knightMoves = (start, end) => {
-    
+    const moves = [
+        [-1, -2],
+        [-1, +2],
+        [+1, -2],
+        [+1, +2],
+        [-2, -1],
+        [-2, +1],
+        [+2, -1],
+        [+2, +1]
+    ]
+
+    let queue = [start];
+    let parents = new Map();
+    let visited = [start];
+
+    while(queue.length > 0){
+        let currentPos = queue.shift();
+
+        for(let move of moves){
+            let newRow = currentPos[0] + move[0];
+            let newCol = currentPos[1] + move[1];
+
+            if(newRow >= 0 && newRow <= 7){
+                if(newCol >= 0 && newCol <= 7){
+                    let destination = [newRow, newCol];
+
+                    if(!visited.some(visitedPos => visitedPos[0] === destination[0] && visitedPos[1] === destination[1])){
+                        visited.push(destination);
+                        parents.set(destination.toString(), currentPos);
+                        queue.push(destination);
+                    }
+                }
+            }
+        }
+
+        if(currentPos[0] === end[0] && currentPos[1] === end[1]){
+            let path = [currentPos];
+
+            while(currentPos[0] !== start[0] || currentPos[1] !== start[1]){
+                let parent = parents.get(currentPos.toString());
+                path.push(parent);
+                currentPos = parent;
+            }
+            path.reverse();
+            return path;
+        } 
+    }
+    return null;
 }
+
+console.log(knightMoves([2,3], [5, 7]));
 
 function buildBoard(){
     let board = [];
@@ -16,7 +65,5 @@ function buildBoard(){
     }
     return board;
 }
-
-console.log(buildBoard());
 
 // console.log(knightMoves([3, 3], [4, 3]))
